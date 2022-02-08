a6cf449db27fee5be897cc94962c07668dc92d61
faheemshai <fahim.shaikh@ibm.com>
2022-02-08 15:25:59 +0530

    testcase

--- test/stages/stage2-mymodule.tf
+++ test/stages/stage2-mymodule.tf
@@ -1,3 +1,10 @@
 module "dev_tools_mymodule" {
   source = "./module"
+  description            = "KMS Keys for Data Encryption"
+  key_spec               = "SYMMETRIC_DEFAULT"
+  rotation_enabled       = "true"
+  enabled                = "true"
+  egion                 = "us-east-1"
+  alias                  = "kms-storage"
+  kms_alias              = "kms-storage-alias"
 }
