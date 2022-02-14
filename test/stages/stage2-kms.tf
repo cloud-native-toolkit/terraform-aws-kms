6b91f9e9a189b2ec009ef52c4e97b112cdc858f4
faheemshai <fahim.shaikh@ibm.com>
2022-02-14 19:31:34 +0530

    latest

---
+++ test/stages/stage2-kms.tf
@@ -0,0 +1,10 @@
+module "dev_kms" {
+  source           = "./module"
+  description      = "KMS Keys for Data Encryption"
+  key_spec         = var.key_spec
+  rotation_enabled = var.rotation_enabled
+  enabled          = var.enabled
+  region           = var.region
+  alias            = var.alias
+  kms_alias        = var.kms_alias
+}
