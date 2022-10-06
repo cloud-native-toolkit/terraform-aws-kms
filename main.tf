d4c97209be869abb4adddb62d8df555d47fda0b5
faheemshai <fahim.shaikh@ibm.com>
2022-10-06 13:42:06 +0530

    update kms policy
    Signed-off-by: faheemshai <fahim.shaikh@ibm.com>

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@ locals {
   resource_group_name   = var.resource_group_name != "" && var.resource_group_name != null ? var.resource_group_name : "default"
   name_prefix_kms           = var.name_prefix != "" && var.name_prefix != null ? var.name_prefix : local.resource_group_name
   kms_name              = var.kms_alias != "" && var.kms_alias != null ? var.kms_alias : "${local.name_prefix_kms}"
+  user_name            = var.user_arn != "" && var.user_arn != null ? var.user_arn : "${data.aws_caller_identity.current.arn}"
 }
 data "aws_caller_identity" "current" {}
 resource "aws_kms_key" "kmskey" {
@@ -32,7 +33,7 @@ resource "aws_kms_key" "kmskey" {
             "Sid": "Allow access for Key Administrators",
             "Effect": "Allow",
             "Principal": {
-                "AWS": "${data.aws_caller_identity.current.arn}"
+                "AWS": "${local.user_name}"
             },
             "Action": [
                 "kms:Create*",
@@ -56,7 +57,7 @@ resource "aws_kms_key" "kmskey" {
             "Sid": "Allow use of the key",
             "Effect": "Allow",
             "Principal": {
-                "AWS": "${data.aws_caller_identity.current.arn}"
+                "AWS": "${local.user_name}"
             },
             "Action": [
                 "kms:Encrypt",
@@ -71,7 +72,7 @@ resource "aws_kms_key" "kmskey" {
             "Sid": "Allow attachment of persistent resources",
             "Effect": "Allow",
             "Principal": {
-                "AWS": "${data.aws_caller_identity.current.arn}"
+                "AWS": "${local.user_name}"
             },
             "Action": [
                 "kms:CreateGrant",
